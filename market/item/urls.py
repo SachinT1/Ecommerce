bd4d3b72741de807c96ec62dc9b8a0296fb911f7
SachinT1 <sachintrivedi2203@gmail.com>
2024-06-27 13:12:20 +0530

    Add files via upload

---
+++ market/item/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path,include
+from . import views
+app_name = 'item'
+urlpatterns=[
+    path('<int:pk>/',views.detail,name='detail'),
+    path('new/',views.new,name='new'),
+    path('browse/',views.browse,name='browse'),
+    path('<int:pk>/delete/',views.delete,name='delete'),
+    path('<int:pk>/edit/',views.edit,name='edit'),
+]
