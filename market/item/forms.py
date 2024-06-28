1481ec001cc63b5f18a5ac144fbfc860e2590de9
SachinT1 <sachintrivedi2203@gmail.com>
2024-06-28 10:41:39 +0530

    Update forms.py

--- market/item/forms.py
+++ market/item/forms.py
@@ -1,7 +1,37 @@
 from django import forms
 from .models import *
+from django.core.exceptions import ValidationError
 
 INPUT_CLASSES='w-1/2 py-4 px-6 rounded-xl border'
+
+class NewCatForm(forms.ModelForm):
+    """class Meta:
+        model=Category
+        fields = ('name',)
+        widgets={
+            'name':forms.TextInput(attrs={
+                'class':INPUT_CLASSES
+            }),
+        }"""
+    class Meta:
+        model = Category
+        fields=('name',)
+        widgets={
+            'name':forms.TextInput(attrs={
+                'class':INPUT_CLASSES
+            }),
+        }
+
+    name = forms.CharField(max_length=255)
+
+    def clean(self):
+        cleaned_data= super(NewCatForm,self).clean()
+        catname = cleaned_data.get('name')
+        if validate_text(catname):
+            self.add_error("name","This Category already exists")  
+        return cleaned_data
+
+
 
 class NewItemForm(forms.ModelForm):
     class Meta:
